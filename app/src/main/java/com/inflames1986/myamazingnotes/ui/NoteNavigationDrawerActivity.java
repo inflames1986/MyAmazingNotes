1073921153e278c33637c4316ded9e36afef633e
TMichael <js5859168@gmail.com>
2021-09-07 17:38:47 +0300

    Creating menus for lesson 7 of Android processing

---
+++ app/src/main/java/com/inflames1986/myamazingnotes/ui/NoteNavigationDrawerActivity.java
@@ -0,0 +1,16 @@
+package com.inflames1986.myamazingnotes.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.inflames1986.myamazingnotes.R;
+
+public class NoteNavigationDrawerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_note_navigation_drawer);
+    }
+}
