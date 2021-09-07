4b46f93dc446ae061a238317784c704277c90f5a
TMichael <js5859168@gmail.com>
2021-09-07 17:41:56 +0300

    Creating menus for lesson 7 of Android processing, NoteActivity updated

--- app/src/main/java/com/inflames1986/myamazingnotes/ui/NoteNavigationDrawerActivity.java
+++ app/src/main/java/com/inflames1986/myamazingnotes/ui/NoteNavigationDrawerActivity.java
@@ -2,15 +2,37 @@ package com.inflames1986.myamazingnotes.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.inflames1986.myamazingnotes.R;
+import com.inflames1986.myamazingnotes.domain.Note;
+import com.inflames1986.myamazingnotes.ui.details.NoteDetailsActivity;
+import com.inflames1986.myamazingnotes.ui.details.NoteDetailsFragment;
+import com.inflames1986.myamazingnotes.ui.list.NotesListFragment;
 
-public class NoteNavigationDrawerActivity extends AppCompatActivity {
+public class NoteNavigationDrawerActivity extends AppCompatActivity implements NotesListFragment.OnNoteClicked {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note_navigation_drawer);
+    }
+
+    @Override
+    public void onNoteOnClicked(Note note) {
+
+        if (getResources().getBoolean(R.bool.isLandscape)) {
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.details_container, NoteDetailsFragment.newInstance(note), null)
+                    .commit();
+
+        } else {
+            Intent intent = new Intent(this, NoteDetailsActivity.class);
+            intent.putExtra(NoteDetailsActivity.ARG_NOTE, note);
+            startActivity(intent);
+        }
     }
 }
