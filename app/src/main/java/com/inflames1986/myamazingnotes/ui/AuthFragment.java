1ea9c621559b4de70eb906a4c9519e74bafcf915
TMichael <js5859168@gmail.com>
2021-09-22 16:52:27 +0300

    Lesson 10 Firestore added, google auth add

--- app/src/main/java/com/inflames1986/myamazingnotes/ui/AuthFragment.java
+++ app/src/main/java/com/inflames1986/myamazingnotes/ui/AuthFragment.java
@@ -1,31 +1,70 @@
 package com.inflames1986.myamazingnotes.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.tasks.Task;
 import com.inflames1986.myamazingnotes.R;
+import com.inflames1986.myamazingnotes.ui.list.NotesListFragment;
 
 public class AuthFragment extends Fragment {
 
-    public AuthFragment () {
+    public AuthFragment() {
 
         super(R.layout.fragment_auth);
     }
+
+    private final ActivityResultLauncher<Intent> launcher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+        @Override
+        public void onActivityResult(ActivityResult result) {
+
+            Task<GoogleSignInAccount> accountTask = GoogleSignIn.getSignedInAccountFromIntent(result.getData());
+
+            if (accountTask.isSuccessful()) {
+                GoogleSignInAccount account = accountTask.getResult();
+
+                getChildFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, new NotesListFragment())
+                        .addToBackStack(null)
+                        .commit();
+            }
+
+        }
+    });
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.btn_sign_in).setOnLongClickListener(new View.OnLongClickListener() {
+
+        GoogleSignInOptions signInOptions = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
+
+        GoogleSignInClient client = GoogleSignIn.getClient(requireContext(), signInOptions);
+
+
+        view.findViewById(R.id.btn_sign_in).setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onLongClick(View view) {
-                return false;
+            public void onClick(View view) {
+
+                launcher.launch(client.getSignInIntent());
+
             }
         });
-
     }
 }
